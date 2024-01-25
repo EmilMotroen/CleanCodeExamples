143e7a184da8d8e56348cde8cfb46e240b5cdb8f
Emil Motrøen <e.motroen@gmail.com>
2024-01-25 16:19:08 +0100

    Changed name of string variable to include type

--- CleanCodeExamples/Comments.cs
+++ CleanCodeExamples/Comments.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanCodeExamples
+﻿namespace CleanCodeExamples
 {
     /// <summary>
     /// Basic implementation of a simple calculator that can perform arithmetics.
--- CleanCodeExamples/MeaningfulNames.cs
+++ CleanCodeExamples/MeaningfulNames.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanCodeExamples
+﻿namespace CleanCodeExamples
 {
     class MeaningfulNames
     {
-        private string _desc;  // Description of ...
+        private string _strDesc;  // Description of ... with type encoding 
 
         public void SetDescription(string description)
         {
-            _desc = description;
+            _strDesc = description;
         }
 
         public string GetDescription()
         {
-            return _desc;
+            return _strDesc;
         }
     }
 }
